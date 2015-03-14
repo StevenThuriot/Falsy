ea75fb3e811af615a15570360e327887d12db800
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2015-03-14 19:21:09 +0100

    Upgrading to latest invoker lib

--- Falsy/Properties/AssemblyInfo.cs
+++ Falsy/Properties/AssemblyInfo.cs
@@ -19,6 +19,7 @@
 #endregion
 
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -57,3 +58,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
+
+[assembly: InternalsVisibleTo("Falsy.Tests")]
