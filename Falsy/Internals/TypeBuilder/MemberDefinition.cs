ca398aba43e1cbbb97ec2574e04ec2fa00dba7ab
Steven Thuriot <StevenThuriot@users.noreply.github.com>
2016-02-03 18:56:31 +0100

    Starting with an instance wrapper
    This will allow existing instances to be wrapped in interfaces (and maybe in the future, have a base class)

--- Falsy/Internals/TypeBuilder/MemberDefinition.cs
+++ Falsy/Internals/TypeBuilder/MemberDefinition.cs
@@ -4,14 +4,6 @@ using Horizon;
 
 namespace Falsy.NET.Internals.TypeBuilder
 {
-    public interface IMemberDefinition
-    {
-        bool IsVirtual { get; }
-        string Name { get; }
-        Type MemberType { get; }
-        void Build(System.Reflection.Emit.TypeBuilder typeBuilder);
-    }
-
     static class MemberDefinition
     {
         public static IMemberDefinition Field(string name, Type type) => new FieldMemberDefinition(name, type);
